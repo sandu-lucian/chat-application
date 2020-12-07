0f8897e45140a803d7024afc35ce77a0feea05e4
Lucian Sandu <dev.luciansandu@gmail.com>
2020-12-07 14:33:32 +0200

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,68 @@
+const socket = io();
+
+document
+  .getElementById("join-chat-btn")
+  .addEventListener("click", function (e) {
+    const input = document.getElementById("username-input");
+    const userName = input.value;
+
+    if (userName.length > 0) {
+      document.getElementById("username-missing").classList.add("display-none");
+      socket.emit("join-chat", userName);
+    } else {
+      document
+        .getElementById("username-missing")
+        .classList.remove("display-none");
+    }
+  });
+
+socket.on("joined-chat", function (userName, userCounter) {
+  document.getElementById("join-chat").classList.add("display-none");
+  document.getElementById("chat-container").classList.remove("display-none");
+  const messageContainer = document.getElementById("chat-messages");
+  const userElement = document.createElement("p");
+  userElement.innerHTML = userName;
+  messageContainer.appendChild(userElement);
+
+  const userCountSpan = document.getElementById("online-users-count");
+  userCountSpan.innerHTML = userCounter;
+});
+
+document
+  .getElementById("send-message-btn")
+  .addEventListener("click", function () {
+    const msgInput = document.getElementById("message");
+    const message = msgInput.value;
+    const colorInput = document.getElementById("msg-color");
+    const msgColor = colorInput.value;
+    socket.emit("send-message", message, msgColor);
+    msgInput.value = "";
+  });
+
+socket.on("new-message", function (message) {
+  const messageContainer = document.getElementById("chat-messages");
+  const messageElement = document.createElement("p");
+  messageElement.innerHTML = message;
+  messageContainer.appendChild(messageElement);
+});
+
+document
+  .getElementById("leave-chat-btn")
+  .addEventListener("click", function () {
+    socket.emit("leave-chat");
+  });
+
+socket.on("left-chat", function (message, userCounter) {
+  const leaveMessageContainer = document.getElementById("chat-messages");
+  const leaveMessageElement = document.createElement("p");
+  leaveMessageElement.innerHTML = message;
+  leaveMessageContainer.appendChild(leaveMessageElement);
+
+  const userCountSpan = document.getElementById("online-users-count");
+  userCountSpan.innerHTML = userCounter;
+});
+
+socket.on("menu", function () {
+  document.getElementById("join-chat").classList.remove("display-none");
+  document.getElementById("chat-container").classList.add("display-none");
+});
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const PORT = 5050;
+
+http.listen(PORT, () => {
+  console.log(`Server started at port ${PORT}`);
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const chatUsers = {};
+
+io.on("connection", (socket) => {
+  console.log("Socket connected -> " + socket.id);
+
+  socket.on("join-chat", function (userName) {
+    chatUsers[socket.id] = userName;
+    socket.join("chat");
+    io.to("chat").emit(
+      "joined-chat",
+      `${userName} has joined the chat!`,
+      `${Object.keys(chatUsers).length} user(s) online!`
+    );
+  });
+
+  socket.on("send-message", function (message, color) {
+    io.to("chat").emit(
+      "new-message",
+      `${chatUsers[socket.id]}: <span style="color:${color}">${message}</span>`
+    );
+  });
+
+  socket.on("leave-chat", function () {
+    let user = chatUsers[socket.id];
+    delete chatUsers[socket.id];
+    io.to("chat").emit(
+      "left-chat",
+      `${user} has left the chat!`,
+      `${Object.keys(chatUsers).length} user(s) online!`
+    );
+    socket.leave("chat");
+    socket.emit("menu");
+  });
+});
